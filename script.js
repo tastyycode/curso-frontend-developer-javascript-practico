d906ee325393b0044c7ad6da8c1b029b3b16e0bf
Carlos C <contact@carloscolin.me>
2022-10-29 01:19:01 -0500

    Product detail

--- script.js
+++ script.js
@@ -1,50 +1,14 @@
 const menuEmail = document.getElementById('navbar-email')
+const productDetailCloseButton = document.getElementById('product-detail-close-btn')
 const hamburgerMenu = document.getElementById('hamburger-menu')
 const navShoppingCartIcon = document.querySelector('.navbar-shopping-cart')
 
 
-const toggleDesktopMenu = () => {
-    const desktopMenu = document.getElementById('desktop-menu')
-    const shoppingCartAside = document.getElementById('cart-detail-container')
+const shoppingCartAside = document.getElementById('cart-detail-container')
+const productDetailAside = document.getElementById('product-detail-container')
+const mobileMenu = document.querySelector('.mobile-menu')
+const desktopMenu = document.getElementById('desktop-menu')
 
-    if (!shoppingCartAside.classList.contains('hidden')) {
-        toggleShoppingCartAside()
-    }
-
-    desktopMenu.classList.toggle('inactive')
-}
-
-const toggleMobileMenu = () => {
-    const mobileMenu = document.querySelector('.mobile-menu')
-    const shoppingCartAside = document.getElementById('cart-detail-container')
-
-    if (!shoppingCartAside.classList.contains('hidden')) {
-        toggleShoppingCartAside()
-    }
-
-    mobileMenu.classList.toggle('hidden')
-}
-
-const toggleShoppingCartAside = () => {
-    const shoppingCartAside = document.getElementById('cart-detail-container')
-    const mobileMenu = document.querySelector('.mobile-menu')
-    const desktopMenu = document.getElementById('desktop-menu')
-
-    if (!mobileMenu.classList.value.includes('hidden')) {
-        toggleMobileMenu()
-    }
-
-    if (!desktopMenu.classList.contains('inactive')) {
-        toggleDesktopMenu()
-    }
-
-    shoppingCartAside.classList.toggle('hidden')
-}
-
-// Event listeners
-menuEmail.addEventListener('click', toggleDesktopMenu)
-hamburgerMenu.addEventListener('click', toggleMobileMenu)
-navShoppingCartIcon.addEventListener('click', toggleShoppingCartAside)
 
 // Products list
 const products = []
@@ -64,21 +28,71 @@ products.push({
     image: 'https://res.cloudinary.com/walmart-labs/image/upload/d_default.jpg/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00019516116544L.jpg'
 })
 
+
+const toggleDesktopMenu = () => {
+    shoppingCartAside.classList.add('hidden')
+    productDetailAside.classList.add('hidden')
+    desktopMenu.classList.toggle('inactive')
+}
+
+const toggleMobileMenu = () => {
+    shoppingCartAside.classList.add('hidden')
+    productDetailAside.classList.add('hidden')
+    mobileMenu.classList.toggle('hidden')
+}
+
+const toggleShoppingCartAside = () => {
+    mobileMenu.classList.add('hidden')
+    desktopMenu.classList.add('inactive')
+    productDetailAside.classList.add('hidden')
+    shoppingCartAside.classList.toggle('hidden')
+}
+
+const openProductDetailAside = () => {
+    desktopMenu.classList.add('inactive')
+    shoppingCartAside.classList.add('hidden')
+    productDetailAside.classList.remove('hidden')
+}
+
+const closeProductDetailAside = () => {
+    productDetailAside.classList.add('hidden')
+}
+
+const createElement = (tag, attributes = {}, children = []) => {
+    const el = document.createElement(tag)
+
+    for (attr in attributes) {
+        el.setAttribute(attr, attributes[attr])
+    }
+
+    if (typeof children === 'object') {
+        for (child of children) {
+            el.appendChild(child)
+        }
+    } else if (typeof children === 'string') {
+        el.innerHTML = children
+    }
+
+    return el
+}
+
 const createProductElement = (product) => {
-    const productElement = document.createElement('div')
-    productElement.classList.add('product-card')
-    productElement.innerHTML = `<img src="${product.image}"
-                    alt="">
-                <div class="product-info">
-                    <div>
-                        <p>$${product.price}</p>
-                        <p>${product.name}</p>
-                    </div>
-                    <figure>
-                        <img src="./icons/bt_add_to_cart.svg" alt="">
-                    </figure>
-                </div>`
-    return productElement
+    return createElement('div', { class: 'product-card' }, [
+        (() => {
+            const img = createElement('img', { src: product.image, title: product.name })
+            img.addEventListener('click', openProductDetailAside)
+            return img
+        })(),
+        createElement('div', { class: 'product-info' }, [
+            createElement('div', {}, [
+                createElement('p', {}, `$${product.price}`),
+                createElement('p', {}, `${product.name}`),
+            ]),
+            createElement('figure', {}, [
+                createElement('img', {src: './icons/bt_add_to_cart.svg', alt: ''})
+            ])
+        ])
+    ])
 }
 
 const renderProducts = (products) => {
@@ -90,3 +104,10 @@ const renderProducts = (products) => {
 }
 
 renderProducts(products)
+
+
+// Event listeners
+menuEmail.addEventListener('click', toggleDesktopMenu)
+hamburgerMenu.addEventListener('click', toggleMobileMenu)
+navShoppingCartIcon.addEventListener('click', toggleShoppingCartAside)
+productDetailCloseButton.addEventListener('click', closeProductDetailAside)
